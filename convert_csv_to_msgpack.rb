50a66907cb69ba6513f071120872f70622120d89
toru-t <ttakahashi@sios.com>
2013-04-19 13:52:45 +0900

    first commit

---
+++ convert_csv_to_msgpack.rb
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+require 'msgpack' # need to install
+require 'optparse' # コマンドラインの引数取得用
+require 'csv'
+require 'zlib' # gip用
+
+#
+# csv形式からMsgPack形式に変換するスクリプト
+# csvの先頭にheader情報が付与されているものとする
+# Author: T.Takahashi
+# Date  : 2013/04/19
+#
+
+# コマンドライン引数の取得
+def option
+  return OptionParser.new do |parser|
+    parser.on('-i', '--input INPUT_FILE', 'csv format, input file path.')
+  end.getopts
+end
+
+# ヘッダー情報の取得
+infile = CSV.open(option['input'], 'rb')
+
+# gzipの作成
+outfile = File.open("aed.csv.msgpack.gz", "wb")
+gz = Zlib::GzipWriter.new(outfile)
+
+# header情報の読み込みとheader分のシフト
+header = infile.take(1)[0]
+
+# CSVファイルからの読み込み
+infile.each do |row|
+  i = 0
+  # 毎rowでHashの初期化
+  data = Hash::new
+  
+  # 各columnの情報からhashの作成
+  row.each do |item|
+    data[header[i]] = item
+    # 次のheaderへ
+    i = i + 1
+  end
+  
+  # 今回はcsvに時間情報が無いため付与する. とりあえず現在の時刻のunixtimeを格納する
+  data['time'] = Time.now.to_i
+  
+  # msgpackに変換し、書き込み
+  gz.write(data.to_msgpack)
+end
+
+gz.close
+infile.close
+
+puts("done.")
